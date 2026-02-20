a2e378f03215e0e2c239c0e12c224874a7faf7d1
ganishahfahad2005 <ganishahfahad2005@gmail.com>
2026-02-20 10:08:31 +0530

    shah fahad solved usecase1

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,12 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome Checker ");
+        System.out.println("=================================");
+        System.out.println("This program checks whether a word");
+        System.out.println("or number is a palindrome or not.");
+        System.out.println("Enter your input to begin!");
+    }
+}
